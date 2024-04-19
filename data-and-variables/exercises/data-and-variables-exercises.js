47a7f30c7ce67f6c5d31ceb76edc55dc07b987c3
Julie <jamickael@gmail.com>
2024-04-19 15:18:59 -0500

    Chapter 4 Exercise Final

--- data-and-variables/exercises/data-and-variables-exercises.js
+++ data-and-variables/exercises/data-and-variables-exercises.js
@@ -9,3 +9,37 @@
 // Calculate a trip to the moon below
 
 // Print the results of the trip to the moon below
+
+let shuttle = "Determination"
+let shuttleSpeed = 17500
+let kmToMars = 225000000
+let kmToMoon = 384400
+const milesPerKm = 0.621
+
+console.log(typeof(shuttle))
+console.log(typeof(shuttleSpeed))
+console.log(typeof(kmToMars))
+console.log(typeof(kmToMoon))
+console.log(typeof(milesPerKm))
+
+let milesToMars = kmToMars * milesPerKm
+// console.log(milesToMars);
+
+let hoursToMars = milesToMars / shuttleSpeed
+// console.log(hoursToMars);
+
+let daysToMars = hoursToMars / 24
+// console.log(daysToMars)
+
+console.log(shuttle, "will take", daysToMars, "days to reach Mars.")
+
+let milesToMoon = kmToMoon * milesPerKm
+// console.log(milesToMoon);
+
+let hoursToMoon = milesToMoon / shuttleSpeed
+// console.log(hoursToMoon);
+
+let daysToMoon = hoursToMoon / 24
+// console.log(daysToMoon);
+
+console.log(shuttle, "will take", daysToMoon, "days to reach the Moon.");
