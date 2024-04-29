296e4685234a504493977ded63ad51d1e922253b
Julie <jamickael@gmail.com>
2024-04-29 14:42:37 -0500

    Debugging Logic 3 update

--- errors-and-debugging/exercises/DebuggingLogicErrors3.js
+++ errors-and-debugging/exercises/DebuggingLogicErrors3.js
@@ -25,6 +25,8 @@ if (crewStatus && computerStatus === 'green'){
    console.log('WARNING: Crew or computer not ready!');
    launchReady = false;
 }
+console.log(launchReady); //launchReady should now be 'True'. The program is behaving as expected for this else/if.
+
 
 // if (launchReady) {
 //    console.log('10, 9, 8, 7, 6, 5, 4, 3, 2, 1...');
