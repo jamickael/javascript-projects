d7f7c07cb26cb903e1f15a9b985ee8ba9e1f6d9a
Julie <jamickael@gmail.com>
2024-04-29 14:47:29 -0500

    Debugging Logic 4 update

--- errors-and-debugging/exercises/DebuggingLogicErrors4.js
+++ errors-and-debugging/exercises/DebuggingLogicErrors4.js
@@ -2,7 +2,9 @@
 // Run the code and examine the output. 
 
 // Given the values for fuelLevel, crewStatus and computerStatus, should launchReady be true or false?
-// Is the program behaving as expected?
+//    False, then True, and ultimately True by the end of the program. 
+// Is the program behaving as expected? 
+//    The program is not bahving as expected. Both else/if statements are manipulating the same variable
 
 let launchReady = false;
 let fuelLevel = 17000;
@@ -17,7 +19,7 @@ if (fuelLevel >= 20000) {
    launchReady = false;
 }
 
-console.log("launchReady = ", launchReady);
+console.log("launchReady = ", launchReady); // this correctly produces a False answer.
 
 if (crewStatus && computerStatus === 'green'){
    console.log('Crew & computer cleared.');
@@ -27,7 +29,7 @@ if (crewStatus && computerStatus === 'green'){
    launchReady = false;
 }
 
-console.log("launchReady = ", launchReady);
+console.log("launchReady = ", launchReady); // this correctly produced a True answer. 
 
 // if (launchReady) {
 //    console.log('10, 9, 8, 7, 6, 5, 4, 3, 2, 1...');
