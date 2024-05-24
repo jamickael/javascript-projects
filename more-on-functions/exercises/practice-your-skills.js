052bc2a50cf63bed0a475047e41ef32935b9e464
Julie <jamickael@gmail.com>
2024-05-24 14:21:06 -0500

    ex 1 update

--- more-on-functions/exercises/practice-your-skills.js
+++ more-on-functions/exercises/practice-your-skills.js
@@ -5,11 +5,25 @@ a) If passed a number, return the tripled value.
 b) If passed a string, return the string “ARRR!”
 c) Be sure to test your function before moving on the next part. 
 */
+let triplePirateBooty = function(input) {
+    if (Number(input)) {
+        return input * 3;
+    } else {
+        return "Arrrr!";
+    }
+};
+console.log(triplePirateBooty("pirate"));
 
 /* Add to your code! Use your fuction and the map method to change an array  as follows:
 a) Triple any the numbers.
 b) Replace any strings with “ARRR!”
 c) Print the new array to confirm your work.
 */
-
 let arr = ['Elocution', 21, 'Clean teeth', 100];
+let changedArray = arr.map(triplePirateBooty);
+console.log(arr);
+console.log(changedArray);
+
+
+
+
