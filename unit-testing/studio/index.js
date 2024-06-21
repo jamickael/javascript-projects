a028c2ba1941eafe61806778f2352f98274b232a
Julie <jamickael@gmail.com>
2024-06-20 20:24:49 -0500

    studio update

--- unit-testing/studio/index.js
+++ unit-testing/studio/index.js
@@ -1,7 +1,24 @@
 
 let launchcode = {
-  
+  organization : "nonprofit",
+  executiveDirector : "Jeff",
+  percentageCoolEmployees : 100,
+  programsOffered : ["Web Development", "Data Analysis", "Liftoff"],
+  launchOutput : launchOutput
 }
 
-module.exports = launchcode;
+function launchOutput(num) {
+    if (num % 2 === 0) {
+        return 'Launch!';
+    } if else (num % 3 === 0) {
+        return 'Code!';
+    } else {
+        return 'No launch';
+    }
+  }
 
+
+module.exports = {
+    launchcode : launchcode,
+    launchOutput : launchOutput
+}
--- unit-testing/studio/tests/launchcode.test.js
+++ unit-testing/studio/tests/launchcode.test.js
@@ -2,7 +2,25 @@
 const launchcode = require('../index.js');
 
 describe("Testing launchcode", function(){
+  test("test that org key returns 'nonprofit'", function() {
+    expect(launchcode.launchcode.organization).toBe("nonprofit");
+  })
+  test("test that executiveDirector key returns 'Jeff'", function() {
+    expect(launchcode.launchcode.executiveDirector).toBe("Jeff");
+  })
+  test("test that percentageCoolEmployees is 100%", function() {
+    expect(launchcode.launchcode.percentageCoolEmployees).toBe(100);
+  })
+  test('test that programsOffered returns ["Web Development", "Data Analysis", "Liftoff"] properly', function() {
+    expect(launchcode.launchcode.programsOffered[0]).toBe("Web Development");
+    expect(launchcode.launchcode.programsOffered[1]).toBe("Data Analysis");
+    expect(launchcode.launchcode.programsOffered[2]).toBe("Liftoff");
+    expect(launchcode.launchcode.programsOffered.length).toEqual(3);
+  })
+  test("test that launchOutput returns the proper phrases", function() {
+    expect(launchcode.launchOutput(2)).toBe('Launch!');
+    expect(launchcode.launchOutput(3)).toBe('Code!');
 
-  // Write your unit tests here!
-  
+  })
+
 });
